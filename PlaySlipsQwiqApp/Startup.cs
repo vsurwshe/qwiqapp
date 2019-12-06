80182e79fc29a0ccaaacad8329c6f2f220f405e5
Vishvanath Surwashe <vsurwshe@gmail.com>
2019-12-06 17:51:25 +0530

    0.0.0 : Code initiate

---
+++ PlaySlipsQwiqApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PlaySlipsQwiqApp.Startup))]
+namespace PlaySlipsQwiqApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
